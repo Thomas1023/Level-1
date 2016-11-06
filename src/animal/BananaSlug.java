0916f8d746003b976462fb6231ead44feeee8779
Garrett-Thomas <league@WTS10.attlocal.net>
2016-11-06 12:59:53 -0800

    t
    t

---
+++ src/animal/BananaSlug.java
@@ -0,0 +1,40 @@
+package animal;
+
+public class BananaSlug {
+String favoriteFood;
+int numberOfFreineds;
+int numberOfChopsticks;
+String colorOfSkin;
+int slugyness;
+int comrodery;
+String personality;
+int size;
+char favoriteLetter;
+int favoriteNumber;
+
+public BananaSlug(String favoriteFood, int numberOfFreineds, int numberOfChopsticks, String colorOfSkin, int slugyness,
+		int comrodery, String personality, int size, char favoriteLetter, int favoriteNumber) {
+	super();
+	this.favoriteFood = favoriteFood;
+	this.numberOfFreineds = numberOfFreineds;
+	this.numberOfChopsticks = numberOfChopsticks;
+	this.colorOfSkin = colorOfSkin;
+	this.slugyness = slugyness;
+	this.comrodery = comrodery;
+	this.personality = personality;
+	this.size = size;
+	this.favoriteLetter = favoriteLetter;
+	this.favoriteNumber = favoriteNumber;
+}
+
+
+
+public void Slug(){
+System.out.println("slug");
+}
+public void Banana(){
+	System.out.println("banana");
+}
+
+
+}
---
+++ src/animal/Runner.java
@@ -0,0 +1,10 @@
+package animal;
+
+public class Runner {
+public static void main(String[] args) {
+	BananaSlug Phil = new BananaSlug("grass",-50,132,"pink",42,9001,"mean",60,'g',42);
+	
+	Phil.Slug();
+	Phil.Banana();
+}
+}
